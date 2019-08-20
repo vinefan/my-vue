0b4c5fea08c392170917e6fd771d7f4fa4596c38
vinefan <17360078826@163.com>
2019-08-20 15:21:02 +0800

    add a mouseover action

--- my-vue/src/components/Header.vue
+++ my-vue/src/components/Header.vue
@@ -2,18 +2,17 @@
     <div id="header">
     <div id="logo"><i class="el-icon-s-tools"></i><div>善行保</div></div>
     <div id="menu">
-        <a  href="#"
-            v-on:onmouseenter="changeColor" 
-            v-bind:class="insure-link">
-            <i class="el-icon-tickets"> 投保</i>
-        </a> |
-        <a  href="#"
-            v-on:on>
-            <i class="el-icon-paperclip"> 申述</i>
-        </a> |
-        <a class="vote-link" href="#">
-            <i class="el-icon-edit-outline"> 投票</i>
+        <template v-for="(item, index) in items">
+        <a  href="/"
+            :key="index"
+            @mouseover="changeColor(item)"
+            @mouseout="changeColor(item)"
+            v-bind:class="{ 'active': item.active }">
+            <i :class="item.icon">
+                 {{item.title}}
+            </i>
         </a>
+        </template>
     </div>
     </div>
 </template>
@@ -23,14 +22,29 @@
         name: "Header",
         data: function () {
             return {
-                onColor : "#333",
-                offColor : "#456789",
-                isOver: 0
+                items: [{
+                        icon: "el-icon-tickets",
+                        title: "投保 | ",
+                        active: false
+                    },
+                    {
+                        icon: "el-icon-paperclip",
+                        title: "申述 | ",
+                        active: false
+                    }, {
+                        icon: "el-icon-edit-outline",
+                        title: "投票",
+                        active: false
+                    }
+                ],
+
             }
         },
         methods:{
-            changeColor: function (){
+            changeColor: function (item){
                 
+                item.active = !item.active 
+                // alert(item.active)
             }
         }
     }
@@ -39,7 +53,7 @@
 <style scoped>
     #header{
         height: 90px;
-        background-color: #eeeeee 
+        background-color: #eeeeee
     }
     #logo{
         width: 275px;
@@ -52,7 +66,7 @@
         font-size: 40px;
         height: 100px;
         display: inline-block;
-        
+
     }
     #logo div{
         margin-left: 20px;
@@ -62,16 +76,20 @@
     #menu{
         display: inline-block;
         font-size: 22px;
-        width: 300px;
+        width: 340px;
         height: 90%;
-        margin-left: 40%
+        margin-left: 40%;
+        color: #333
     }
 
-    #menu a{
+    a{
         color: #333;
         display: inline-block;
-        width: 80px;
+        width: 105px;
     }
-    
 
+    .active{
+        color: steelblue;
+        font-size: 23px;
+    }
 </style>
